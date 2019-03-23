9f11d47afa663ef6c9a2e93b3afe208b052d2757
BenasdTW <48852791+BenasdTW@users.noreply.github.com>
2019-03-23 21:12:11 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 
 document.write("wow!!");
+
+function setup(){
+    createCanvas(400, 300);
+    background(120);
+}
+
+function draw(){
+    background(120);
+    
+    fill(200, 100, 0);
+    ellipse(mouseX, mouseY, 30, 30);
+}
